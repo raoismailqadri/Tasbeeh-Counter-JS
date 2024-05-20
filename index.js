b18257fbb3278874aa1d96abd3e8c532562c5579
raoismailqadri <raoismailqadri@gmail.com>
2024-05-20 15:03:16 +0500

    story  is added

--- index.js
+++ index.js
@@ -1,43 +1,32 @@
 isStart = false;
 let count = 0;
 
-const counter = document.getElementById('count');
+const counter = document.getElementById("count");
 
+// SINCE,counter() IS OUT OF FUNCTION ,HENCE TREATED AS GLOBAL METHOD
 counter();
-if (isStart == false){
-
-    isStart = true;
+if (isStart == false) {
+  isStart = true;
 }
 
-
-function increment()
-{
-    if (isStart = true){
-        
-        count++;
-    }
-    counter.innerHTML =  count;
-}
-     
-
-function decrement()
-{
-    if (isStart = true){
-        
-        count--;
-    }
-    counter.innerText=count;
+function increment() {
+  if ((isStart = true)) {
+    count++;
+  }
+  counter.innerHTML = count;
 }
 
-   
+function decrement() {
+  if ((isStart = true)) {
+    count--;
+  }
+  counter.innerText = count;
+}
 
+function reset() {
+  if ((isStart = true)) {
+    count = 0;
+  }
 
-function reset()
-{
-    if (isStart = true){
-
-        count=0;
-    }
-        
-    counter.innerText=count;
+  counter.innerText = count;
 }
